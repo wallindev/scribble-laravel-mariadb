89c1e684527f63b8d97fbdd99d6b9791b5e2ae6b
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-01-07 20:17:12 +0100

    Added home layout

---
+++ resources/views/layouts/home.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Home - Scribble.dev</title>
+  <!-- Add your CSS and JS links here -->
+</head>
+<body>
+  <div class="container">
+    @yield('content')
+  </div>
+</body>
+</html>
