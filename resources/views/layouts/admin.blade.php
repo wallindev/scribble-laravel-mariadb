f98b4445de75dacac1aa856a69e3c0c034d5bf04
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-01-09 21:38:09 +0100

    Added some basic info, used special Vite directive to add stylesheet and javascript resources, and fixed indentation

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,11 +1,15 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Admin - Scribble!</title>
-</head>
-<body>
-  <div class="container">
-    @yield('content')
-  </div>
-</body>
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="icon" type="image/svg+xml" href="/img/scribble-bold.svg" />
+    @vite(['resources/css/app.module.styl', 'resources/js/app.js'])
+    <title>Admin - {{ config('app.name', 'Scribble! (from Blade)') }}</title>
+  </head>
+  <body>
+    <div class="container">
+      @yield('content')
+    </div>
+  </body>
 </html>
