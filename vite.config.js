36a8eff71fcf1b4261ea877cd0e6aa3dd61ebfcf
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-01-10 23:10:12 +0100

    Changed to correct indentation

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,15 @@ import laravel from 'laravel-vite-plugin'
 import stylus from 'stylus'
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.module.styl', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        stylus({
-            modules: {
-                localsConvention: 'camelCase',
-            },
-        }),
-    ],
+  plugins: [
+    laravel({
+      input: ['resources/css/app.module.styl', 'resources/js/app.js'],
+      refresh: true,
+    }),
+    stylus({
+      modules: {
+        localsConvention: 'camelCase',
+      },
+    }),
+  ],
 })
