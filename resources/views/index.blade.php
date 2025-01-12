d33a7febcdb0bdf37c421f8654d4a398acfb5aa3
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-01-12 21:04:44 +0100

    Added links listing

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,4 +2,13 @@
 
 @section('content')
 <h2>Index Page</h2>
+<table>
+  <tbody>
+  @foreach ($links as $uri => $text)
+    <tr>
+      <td><a href="{{ $uri }}">{{ $text }}</a></td>
+    </tr>
+  @endforeach
+  </tbody>
+</table>
 @endsection
