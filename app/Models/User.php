0f074d98bc7c4418765c523eab30b5bbb6dc4ae2
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-01-13 22:48:01 +0100

    Updated User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,48 +1,68 @@
 <?php
-
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
-{
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+class User extends Authenticatable {
+  /** @use HasFactory<\Database\Factories\UserFactory> */
+  use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
+  // TODO: Should we use Default Attribute Values ($attributes)?
+  // Consideration: You can define default values for model attributes
+  // in the $attributes property. These will be applied when a new model
+  // instance is created but hasn't been saved to the database yet.
+  // protected $attributes = [
+  //   'is_active' => false,
+  //   'settings' => 'default',
+  // ];
+
+  /**
+   * Relations
+   *
+   */
+  public function articles()
+  {
+    return $this->hasMany(Article::class);
+  }
+
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var list<string>
+   */
+  protected $fillable = [
+    'firstname',
+    'lastname',
+    'email',
+    'password',
+    'email_verified',
+  ];
+
+  /**
+   * The attributes that should be hidden for serialization.
+   *
+   * @var list<string>
+   */
+  protected $hidden = [
+    'password',
+    'salt',
+    'remember_token',
+  ];
+
+  /**
+   * Get the attributes that should be cast.
+   *
+   * @return array<string, string>
+   */
+  protected function casts(): array {
+    return [
+      'email_verified'    => 'boolean',
+      'email_verified_at' => 'datetime',
+      'password'          => 'hashed',
     ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
+  }
 }
