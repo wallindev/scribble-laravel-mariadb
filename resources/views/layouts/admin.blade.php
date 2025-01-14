4241d8d8e6052c2e6052808dd2cc0c79c35b01a2
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-01-14 14:55:00 +0100

    Added guard for style sheet link tag generation

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -4,7 +4,13 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="icon" type="image/svg+xml" href="/img/scribble-bold.svg" />
+  @if (file_exists('vite.config.js') || file_exists(public_path('hot')))
     @vite(['resources/css/app.module.styl', 'resources/js/app.js'])
+  @else
+    <style>
+      /* Custom Style */
+    </style>
+  @endif
     <title>Admin - {{ config('app.name', 'Scribble! (from Blade)') }}</title>
   </head>
   <body>
