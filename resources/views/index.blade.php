5e4e98f925c1b0c08a9a22b4ecbe67944c104291
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-01-14 14:50:47 +0100

    Added some boilerplate code

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,14 +1,30 @@
 @extends('layouts.home')
 
 @section('content')
+<header class="header">
+  @if (Route::has('login'))
+  <nav class="nav">
+    @auth
+    <a href="{{ url('/dashboard') }}" class="dashboard">Dashboard </a>
+  @else
+    <a href="{{ route('login') }}" class="login">Log in</a>
+    @if (Route::has('register'))
+    <a href="{{ route('register') }}" class="register">Register</a>
+    @endif
+    @endauth
+  </nav>
+  @endif
+</header>
 <h2>Index Page</h2>
 <table>
   <tbody>
-  @foreach ($links as $uri => $text)
+    @foreach ($links as $uri => $text)
     <tr>
-      <td><a href="{{ $uri }}">{{ $text }}</a></td>
+      <td>
+        <a href="{{ $uri }}">{{ $text }}</a>
+      </td>
     </tr>
-  @endforeach
+    @endforeach
   </tbody>
 </table>
 @endsection
