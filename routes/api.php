132b15dd4195cf2fb0688267ad2259056b0ea531
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-01-15 15:58:17 +0100

    Added articles API route (/api/articles)

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Models\Article;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/articles', function () {
+    $articles = Article::all();
+    return response()->json($articles);
+});
