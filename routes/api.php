55bfa846e6ee53036dc602c05ca7b81d9b6f7e5a
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-01-15 17:05:54 +0100

    Commenting out user route with Sanctum for now, returning to this later

--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,12 @@
 
 use App\Models\Article;
 use App\Models\User;
-use Illuminate\Http\Request;
+// use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
 Route::get('/articles', function () {
     $articles = Article::all();
