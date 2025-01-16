98ad652b5b782ee785fafe447797b5c195b9cd21
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-01-16 01:41:44 +0100

    Updated root/home route to call index function in the HomeController class (which in turn returns the index view)

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,31 @@
 <?php
 
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('index');
+Route::get('/', [HomeController::class, 'index']);
+
+// Route::get(
+//   '/user/profile',
+//   [UserProfileController::class, 'show']
+// )->name('profile');
+
+// // Generating URLs...
+// $url = route('profile');
+
+// // Generating Redirects...
+// return redirect()->route('profile');
+
+// return to_route('profile');
+
+// Route::get('/', function () {
+//     return view('index');
+// });
+
+Route::get('/greeting', function () {
+  return 'Hello World';
 });
+
+Route::view('/welcome', 'welcome');
