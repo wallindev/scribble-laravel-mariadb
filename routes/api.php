743fbe24dd80f735fee35cde80807dca385c971a
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-01-17 01:56:01 +0100

    Fixed formatting

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Models\Article;
 use App\Models\User;
+use Illuminate\Support\Facades\Log;
 // use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -11,24 +12,24 @@ use Illuminate\Support\Facades\Route;
 
 // All Articles
 Route::get('/articles', function () {
-    $articles = Article::all();
-    return response()->json($articles);
+  $articles = Article::all();
+  return response()->json($articles);
 });
 
 // All users
 Route::get('/users', function () {
-    $users = User::all();
-    return response()->json($users);
+  $users = User::all();
+  return response()->json($users);
 });
 
 // One Article
 Route::get('/articles/{id}', function (string $id) {
-    $article = Article::find($id);
-    return response()->json($article);
+  $article = Article::find($id);
+  return response()->json($article);
 })->where('id', '[0-9]+');
 
 // One User
 Route::get('/users/{id}', function (string $id) {
-    $user = User::find($id);
-    return response()->json($user);
+  $user = User::find($id);
+  return response()->json($user);
 })->where('id', '[0-9]+');
