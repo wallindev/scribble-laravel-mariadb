eb885ac6231dab8038ebf7e0b995c5c35790589e
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-01-19 02:46:37 +0100

    Added index, show, and edit functions

--- app/Http/Controllers/Admin/ArticleController.php
+++ app/Http/Controllers/Admin/ArticleController.php
@@ -2,5 +2,31 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\Article;
+
 class ArticleController extends AdminController {
+  public function index() {
+    $articles = Article::all();
+    return view('admin.articles.index', [
+      'title'    => 'Articles',
+      'articles' => $articles,
+    ]);
+    // return view('admin.articles.index', compact('articles'));
+  }
+
+  public function show($id) {
+    $article = Article::findOrFail($id);
+    return view('admin.articles.show', [
+      'title'   => 'Show Article',
+      'article' => $article,
+    ]);
+  }
+
+  public function edit($id) {
+    $article = Article::findOrFail($id);
+    return view('admin.articles.edit', [
+      'title'   => 'Edit Article',
+      'article' => $article,
+    ]);
+  }
 }
