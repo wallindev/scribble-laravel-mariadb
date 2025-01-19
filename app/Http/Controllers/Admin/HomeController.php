3e5a25b4cc4499bd029e3d1d75d7195fe228b08c
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-01-19 02:49:11 +0100

    Added index function

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -3,4 +3,13 @@
 namespace App\Http\Controllers\Admin;
 
 class HomeController extends AdminController {
+  public function index() {
+    return view('admin.index', [
+      'title' => 'Start',
+      'links' => [
+        'admin/users'    => 'Users',
+        'admin/articles' => 'Articles',
+      ],
+    ]);
+  }
 }
