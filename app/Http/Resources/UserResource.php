92226ffa8243fe99251ddfd447ff60c3830257a9
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-01-20 06:59:28 +0100

    Added toArray function

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -5,4 +5,13 @@ namespace App\Http\Resources;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class UserResource extends JsonResource {
+  public function toArray($request) {
+    return [
+      'id'          => $this->id,
+      'firstName'   => $this->firstname,
+      'lastName'    => $this->lastname,
+      'email'       => $this->email,
+      // Include any other fields or relationships
+    ];
+  }
 }
