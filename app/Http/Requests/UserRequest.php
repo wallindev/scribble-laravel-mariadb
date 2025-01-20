b43546267e650146d3ad83ee4e1213e12b945d56
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-01-20 05:43:39 +0100

    Added attributes, authorize, and rules functions

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -5,4 +5,25 @@ namespace App\Http\Requests;
 use Illuminate\Foundation\Http\FormRequest;
 
 class UserRequest extends FormRequest {
+  public function attributes() {
+    return [
+      'firstname' => 'first name',
+      'lastname'  => 'full name',
+      'email'     => 'email address',
+      // 'password' => 'password',
+    ];
+  }
+
+  public function authorize() {
+    return true; // You can implement authorization logic here
+  }
+
+  public function rules() {
+    dump('UserRequest->rules()');
+    return [
+      // 'name' => 'required|string|max:255',
+      // 'email' => 'required|string|email|max:255|unique:users',
+      // 'password' => 'required|string|min:8',
+    ];
+  }
 }
