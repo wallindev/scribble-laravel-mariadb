e73557797b8b8b9710667a353fbc289ad21b0d50
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-01-21 07:12:13 +0100

    Added register function, binding with UserServiceInterface and UserService

--- app/Providers/UserServiceProvider.php
+++ app/Providers/UserServiceProvider.php
@@ -2,7 +2,26 @@
 
 namespace App\Providers;
 
+use App\Services\UserService;
+use App\Services\UserServiceInterface;
 use Illuminate\Support\ServiceProvider;
 
 class UserServiceProvider extends ServiceProvider {
+  /**
+   * Register services.
+   *
+   * @return void
+   */
+  public function register() {
+    $this->app->bind(UserServiceInterface::class, UserService::class);
+  }
+
+  /**
+   * Bootstrap services.
+   *
+   * @return void
+   */
+  public function boot() {
+    //
+  }
 }
