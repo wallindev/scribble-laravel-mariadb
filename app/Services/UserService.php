8b4d63b99cdf9a0280c2a5abcde957a6385393b5
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-01-21 07:18:59 +0100

    Added all functions defined in UserServiceInterface

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -2,5 +2,41 @@
 
 namespace App\Services;
 
+use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
+
 class UserService implements UserServiceInterface {
+  // For API
+  // public function getAllUsers(): array
+  // For GUI
+  public function getAllUsers(): Collection {
+    // For API
+    // return User::all()->toArray();
+    // For GUI
+    return User::all();
+  }
+
+  public function getUserById(int $id): ?User {
+    return User::find($id);
+  }
+
+  public function createUser(array $data): User {
+    return User::create($data);
+  }
+
+  public function updateUser(int $id, array $data): bool {
+    $user = $this->getUserById($id);
+    return $user ? $user->update($data) : false;
+  }
+
+  public function deleteUser(int $id): bool {
+    $user = $this->getUserById($id);
+    return $user ? $user->delete() : false;
+  }
+
+  public function isAdmin(): bool {
+    // TODO: How get is_admin from db? Implement in User model? Is '$this' referring to the model?
+    return false;
+    // return $this->is_admin;
+  }
 }
