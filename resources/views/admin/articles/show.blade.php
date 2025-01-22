0d1f22981471b7318358f0ceeb24224e27942aa1
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-01-22 20:16:35 +0100

    Added content for article show view

--- resources/views/admin/articles/show.blade.php
+++ resources/views/admin/articles/show.blade.php
@@ -1,4 +1,9 @@
 @extends('layouts.admin')
 
 @section('content')
+    <h2>Article Details</h2>
+    <p><strong>ID:</strong> {{ $article->id }}</p>
+    <p><strong>Title:</strong> {{ $article->title }}</p>
+    <p><strong>Content:</strong> {{ $article->content }}</p>
+    <a href="{{ route('admin.articles.edit', $article->id) }}">Edit Article</a>
 @endsection
