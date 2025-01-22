4ba6c830808b88d832bcaa3fd16066fdcd4d6bb3
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-01-22 20:20:19 +0100

    Added form for editing an article in the edit view

--- resources/views/admin/articles/edit.blade.php
+++ resources/views/admin/articles/edit.blade.php
@@ -1,4 +1,18 @@
 @extends('layouts.admin')
 
 @section('content')
+    <h2>Edit Article</h2>
+    <form action="{{ route('admin.articles.update', $article->id) }}" method="POST">
+        @csrf
+        @method('PUT') <!-- Use PUT method for updating -->
+        <div>
+            <label for="title">Title:</label>
+            <input type="text" id="title" name="title" value="{{ $article->title }}" required>
+        </div>
+        <div>
+            <label for="content">Content:</label>
+            <textarea id="content" name="content" required>{{ $article->content }}</textarea>
+        </div>
+        <button type="submit">Update Article</button>
+    </form>
 @endsection
