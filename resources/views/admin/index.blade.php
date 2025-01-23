8f15ccb320353ae87d0baac0a9b0b8c6b3c8b757
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-01-23 20:29:36 +0100

    Added content for admin index view

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,4 +1,14 @@
 @extends('layouts.admin')
 
 @section('content')
+<h2>Admin Start</h2>
+<table>
+  <tbody>
+  @foreach ($links as $uri => $text)
+    <tr>
+      <td><a href="{{ $uri }}">{{ $text }}</a></td>
+    </tr>
+  @endforeach
+  </tbody>
+</table>
 @endsection
