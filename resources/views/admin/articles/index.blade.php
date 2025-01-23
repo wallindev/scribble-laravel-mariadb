f3c2f96b218d138ee05669be5515f56e600a5a88
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-01-23 20:25:31 +0100

    Added listing table in articles index view

--- resources/views/admin/articles/index.blade.php
+++ resources/views/admin/articles/index.blade.php
@@ -1,4 +1,27 @@
 @extends('layouts.admin')
 
 @section('content')
+<h2>Articles</h2>
+<table>
+  <thead>
+    <tr>
+      <th>ID</th>
+      <th>Headline</th>
+      <th>Content</th>
+      <th>Actions</th>
+    </tr>
+  </thead>
+  <tbody>
+  @foreach ($articles as $article)
+    <tr>
+      <td>{{ $article->id }}</td>
+      <td>{{ $article->headline }}</td>
+      <td>{{ $article->content }}</td>
+      <td>
+        <!-- Add action buttons here (edit, delete, etc.) -->
+      </td>
+    </tr>
+  @endforeach
+  </tbody>
+</table>
 @endsection
