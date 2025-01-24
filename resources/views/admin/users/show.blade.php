c08a8fa970b75c83a170a2c4dd7c79b379001e0e
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-01-24 20:38:19 +0100

    Added content for users show view

--- resources/views/admin/users/show.blade.php
+++ resources/views/admin/users/show.blade.php
@@ -1,4 +1,27 @@
 @extends('layouts.admin')
 
 @section('content')
+<h2>User Details</h2>
+@if($success)
+<div class="text-primary"><p>{{ $success }}</p></div>
+@endif
+<section>
+<table>
+  <tbody>
+    <tr>
+      <td><strong>ID:</strong></td><td>{{ $user->id }}</td>
+    </tr>
+    <tr>
+      <td><strong>Name:</strong></td><td>{{ $user->name }}</td>
+    </tr>
+    <tr>
+      <td><strong>Email:</strong></td><td>{{ $user->email }}</td>
+    </tr>
+    <tr>
+      <td class="button"><a href="{{ route('users.edit', $user->id) }}" class="btn btn-outline-primary btn-sm" role="button">Edit User</a></td>
+      <td class="button button--right"><a href="{{ route('users.index') }}" class="btn btn-outline-secondary btn-sm" role="button">All Users &raquo;</a></td>
+    </tr>
+  </tbody>
+</table>
+</section>
 @endsection
