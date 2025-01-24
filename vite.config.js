a53bb7a8f285463c9caac1fa8171395aed4e980a
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-01-24 21:12:11 +0100

    Added a few settings, among others set Vite/frontend port to 5000

--- vite.config.js
+++ vite.config.js
@@ -14,4 +14,22 @@ export default defineConfig({
       },
     }),
   ],
+  // build: {
+  //   outDir: 'dist',
+  // },
+  server: {
+    // hmr: false,
+    // host: 'localhost', // 0.0.0.0 for all interfaces
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000', // Local API during dev
+        changeOrigin: true,
+      },
+    },
+    port: 5000,
+    // allowedHosts: ['127.0.0.1', '192.168.32.2', 'grunge', 'grungecorp', 'grunge.dev', 'grungecorp.dev'],
+    watch: {
+      ignored: ['**/api-data/*.*', './TODO.txt'],
+    },
+  },
 })
