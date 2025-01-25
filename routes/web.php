1ea05ee13bd414f4acbf4b6a3739f19de0b514d1
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-01-25 13:54:22 +0100

    Added /admin/users route, listing users

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Route;
 
+// Start Page
 Route::get('/', [HomeController::class, 'index']);
 
+// Admin Start Page
 Route::get('/admin', [Admin\HomeController::class, 'index']);
+
+// List users
+Route::get('/admin/users', [Admin\UserController::class, 'index']);
 
 // Route::get(
 //   '/user/profile',
