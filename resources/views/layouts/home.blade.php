9ae72f382e44f580e8a7d497fb413cc454634e61
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-02-06 20:13:13 +0100

    Added header and footer in home layout view

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -14,8 +14,24 @@
     <title>Home - {{ config('app.name', 'Scribble! (from Blade)') }}</title>
   </head>
   <body>
-    <div class="container">
+    <header>
+      <nav class="main-nav">
+        <div style="display: flex; justify-content: space-between; align-items: center">
+          <div style="font-size: 2rem; font-weight: 600">
+            <a href="{{ route('home.index') }}" title="To Home Start Page">Home</a>
+          </div>
+          <div>Right part of header</div>
+        </div>
+      </nav>
+    </header>
+    <main class="container">
       @yield('content')
-    </div>
+    </main>
+    <footer>
+      <div style="display: flex; justify-content: space-between; align-items: center">
+        <div>&copy; {{ now()->year }} <a href="mailto: wallindev@gmail.com">wallindev</a></div>
+        <div>Right part of footer</div>
+      </div>
+    </footer>
   </body>
 </html>
