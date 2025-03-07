a6303e418aa3058ef3d24c19e29cd877d9b1fdef
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-03-07 16:24:04 +0100

    Removed Roboto font loading, customized and optimized asset loading, added test code with @auth and @endauth for use later

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -4,26 +4,27 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="icon" type="image/svg+xml" href="/img/scribble-bold.svg" />
-    <style>
-      @font-face {
-        font-family: Roboto;
-        font-style: normal;
-        font-weight: 100 900;
-        font-display: swap;
-        src: url(/fonts/Roboto-VariableFont_wdth,wght.ttf) format('truetype');
-      }
-    </style>
-  @if (file_exists('vite.config.js') || file_exists(public_path('hot')))
-    @vite(['resources/css/app.module.styl', 'resources/js/app.js'])
-  @else
-    <style>
-      /* Custom Style */
-    </style>
-  @endif
+@if (file_exists(public_path('hot')))
+  @vite(['resources/css/app.module.styl', 'resources/js/app.js'])
+@else
+@php
+  $manifest = json_decode(file_get_contents(public_path('build/manifest.json')), true);
+  $cssPath = $manifest['resources/css/app.module.styl']['css'][0] ?? null;
+  $cssAssetPath = asset("build/$cssPath");
+  //$jsPath = $manifest['resources/js/app.js']['file'] ?? null;
+  $jsAssetPath = Vite::asset('resources/js/app.js');
+@endphp
+@if ($cssAssetPath)
+    <link rel="stylesheet" href="{{ $cssAssetPath }}">
+@endif
+@if ($jsAssetPath)
+    <script type="module" src="{{ $jsAssetPath }}"></script>
+@endif
+@endif
     <title>Home - {{ config('app.name', 'Scribble! (from Blade)') }}</title>
   </head>
   <body>
-    <header>
+    <header class="header">
       <nav class="main-nav">
         <div style="display: flex; justify-content: space-between; align-items: center">
           <div style="font-size: 2rem; font-weight: 600">
@@ -32,12 +33,21 @@
           <div>Right part of header</div>
         </div>
       </nav>
-    </header>
+    @auth
+      <a href="{{ url('/dashboard') }}" class="dashboard">Dashboard </a>
+    @if (Route::has('login'))
+      <a href="{{ route('login') }}" class="login">Log in</a>
+    @endif
+    @if (Route::has('register'))
+      <a href="{{ route('register') }}" class="register">Register</a>
+    @endif
+    @endauth
+</header>
     <main class="container">
       @yield('content')
     </main>
     <footer>
-      <div style="display: flex; justify-content: space-between; align-items: center">
+      <div style="display: flex; justify-content: space-between; align-items: center; margin: 1rem 0;">
         <div>&copy; {{ now()->year }} <a href="mailto: wallindev@gmail.com">wallindev</a></div>
         <div>Right part of footer</div>
       </div>
