9c2f77bc12d7d8edc6c7a112f7e9628423127257
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-03-10 16:27:49 +0100

    Fixed and removed extraneous settings

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import laravel from 'laravel-vite-plugin'
-import stylus from 'stylus'
 
 export default defineConfig({
   plugins: [
@@ -8,28 +7,25 @@ export default defineConfig({
       input: ['resources/css/app.module.styl', 'resources/js/app.js'],
       refresh: true,
     }),
-    stylus({
-      modules: {
-        localsConvention: 'camelCase',
-      },
-    }),
   ],
-  // build: {
-  //   outDir: 'dist',
-  // },
   server: {
     // hmr: false,
-    // host: 'localhost', // 0.0.0.0 for all interfaces
-    proxy: {
+    host: 'localhost', // 0.0.0.0 for all interfaces
+    /* proxy: {
       '/api': {
         target: 'http://localhost:3000', // Local API during dev
         changeOrigin: true,
       },
-    },
+    }, */
     port: 5000,
     // allowedHosts: ['127.0.0.1', '192.168.32.2', 'grunge', 'grungecorp', 'grunge.dev', 'grungecorp.dev'],
     watch: {
       ignored: ['**/api-data/*.*', './TODO.txt'],
+    },
+  },
+  stylus: {
+    modules: {
+      localsConvention: 'camelCase',
     },
   },
 })
