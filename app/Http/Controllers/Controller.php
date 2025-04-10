795951ce9fafebbcb33e01bdbd47ee8cea540c9f
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-04-10 20:10:37 +0200

    Added $linksHome, $linksAdmin (moved here to the base controller class), added $breadcrumbs base stub

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,11 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+  // Shared breadcrumbs for the whole website
+  protected $linksHome = ['admin' => 'Admin'];
+  protected $linksAdmin = [
+    '/admin/users' => 'Users',
+    '/admin/articles' => 'Articles',
+  ];
+  protected $breadcrumbs = ['/' => 'Home'];
 }
