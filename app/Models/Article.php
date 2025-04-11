3727b3dff7b4d228b67d7c0fc4e45d98bc972809
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-04-11 20:16:33 +0200

    Tidied up Article model

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -13,26 +13,8 @@ class Article extends Model {
   // Traits
   use HasApiTokens, HasFactory, Notifiable;
 
-  // Constants (if any)
-  // protected const string SOME_CONSTANT = 'value';
-
-  // Private or Protected Instance Variables/Properties
-  private $id;
-  private $title;
-  private $content;
-
-  // TODO: Should we use Default Attribute Values ($attributes)?
-  // Consideration: You can define default values for model attributes
-  // in the $attributes property. These will be applied when a new model
-  // instance is created but hasn't been saved to the database yet.
-  // protected $attributes = [
-  //   'is_active' => false,
-  //   'settings' => 'default',
-  // ];
-
   /**
    * Relations
-   *
    */
   public function user() {
     return $this->belongsTo(User::class);
@@ -41,5 +23,17 @@ class Article extends Model {
   /**
    * @var list<string>
    */
-  protected $fillable = ['title', 'content'];
+  protected $fillable = [
+    'title',
+    'content',
+  ];
+
+  /**
+   * @var list<string>
+   */
+  protected $guarded = [
+    'id',
+    'created_at',
+    'updated_at',
+  ];
 }
