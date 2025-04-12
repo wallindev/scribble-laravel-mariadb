4d8bcbc93dacbb75222ee1401c67c34ee755f134
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-04-12 20:28:30 +0200

    Moved UserServiceInterface from Services to Interfaces dir

--- app/Interfaces/UserServiceInterface.php
+++ app/Interfaces/UserServiceInterface.php
@@ -1,18 +1,18 @@
 <?php
 
-namespace App\Services;
+namespace App\Interfaces;
 
 use App\Models\User;
 use Illuminate\Database\Eloquent\Collection;
 
 interface UserServiceInterface {
-  // For API
-  // public function getAllUsers(): array
-  // For GUI
   public function getAllUsers(): Collection;
   public function getUserById(int $id): ?User;
   public function createUser(array $data): User;
   public function updateUser(int $id, array $data): bool;
   public function deleteUser(int $id): bool;
-  public function isAdmin(): bool;
+  // public function isAdmin(int $id): bool;
+
+  // For API
+  // public function getAllUsers(): array
 }
