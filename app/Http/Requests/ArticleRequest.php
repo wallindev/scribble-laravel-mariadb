5536bd9f5b8d1685598833167b9806c87c570eb4
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-04-13 20:32:03 +0200

    Added ArticleRequest

---
+++ app/Http/Requests/ArticleRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ArticleRequest extends FormRequest {
+  public function rules() {
+    $rules_arr = [
+      'title' => 'required|string|max:255',
+      'content' => 'required|string|max:65535',
+    ];
+
+    // dump('$rules_arr:');
+    // dd($rules_arr);
+
+    return $rules_arr;
+  }
+
+  public function authorize() {
+    return true; // You can implement authorization logic here
+  }
+
+  public function attributes() {
+    return [
+      'title' => 'Title',
+      'content' => 'Content',
+    ];
+  }
+}
