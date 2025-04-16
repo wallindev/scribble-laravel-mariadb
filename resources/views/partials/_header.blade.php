6330924f1595a7b067a6e7f1280103b17557d751
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-04-16 20:43:34 +0200

    Added header partial view

---
+++ resources/views/partials/_header.blade.php
@@ -0,0 +1,7 @@
+      <nav class="main-nav">
+        <div style="font-size: 1rem; background-color: #1a1a1a; padding: .25rem .5rem; border-radius: .25rem;">
+          @include('partials._breadcrumbs')
+        </div>
+        <div>Right part of header</div>
+      </nav>
+      @include('partials._auth')
