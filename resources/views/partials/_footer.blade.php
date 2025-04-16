90af3d2a983e12a87222366288b57aad2fce2dd3
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-04-16 20:42:56 +0200

    Added footer partial view

---
+++ resources/views/partials/_footer.blade.php
@@ -0,0 +1,4 @@
+      <div style="display: flex; justify-content: space-between; align-items: center; margin: 1rem 0;">
+        <div>&copy; {{ now()->year }} <a href="mailto: wallindev@gmail.com">wallindev</a></div>
+        <div>Right part of footer</div>
+      </div>
