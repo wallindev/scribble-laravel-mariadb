57ab3072449709728070d5f0d7a9dec94fc9c0e6
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-04-17 20:52:44 +0200

    Set up articles index view in the same design as users index view

--- resources/views/admin/articles/index.blade.php
+++ resources/views/admin/articles/index.blade.php
@@ -2,23 +2,35 @@
 
 @section('content')
 <h2>Articles</h2>
+@if ($success)
+<div class="text-primary"><p>{{ $success }}</p></div>
+@endif
+<section>
+<div style="margin: 1.5rem .5rem;"><a href="{{ route('users.create') }}">Create Article</a></div>
 <table>
   <thead>
-    <tr>
+    <tr class="heading">
       <th>ID</th>
-      <th>Headline</th>
+      <th>Title</th>
       <th>Content</th>
-      <th>Actions</th>
+      <th colspan="3">Actions</th>
     </tr>
   </thead>
   <tbody>
   @foreach ($articles as $article)
+    @php $articleUrl = route('articles.show', $article->id); @endphp
     <tr>
-      <td>{{ $article->id }}</td>
-      <td>{{ $article->headline }}</td>
-      <td>{{ $article->content }}</td>
+      <td><a href="{{ $articleUrl }}">{{ $article->id }}</a></td>
+      <td><a href="{{ $articleUrl }}">{{ $article->title }}</a></td>
+      <td><a href="{{ $articleUrl }}">{{ contentStub($article->content) }}</a></td>
+      <td><a href="{{ $articleUrl }}" class="btn btn-dark btn-sm" role="button">Show</a></td>
+      <td><a href="{{ route('articles.edit', $article->id) }}" class="btn btn-dark btn-sm" role="button">Edit</a></td>
       <td>
-        <!-- Add action buttons here (edit, delete, etc.) -->
+        <form action="{{ route('articles.destroy', $article->id) }}" method="POST" style="display:inline;">
+          @csrf
+          @method('DELETE')
+          <button type="submit" onclick="return confirm('Delete this article?');" class="link-button secondary">Delete</a>
+        </form>
       </td>
     </tr>
   @endforeach
