9cfca6a755143d6f92bf2fd135ba10ac80ddcd12
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-04-19 21:03:39 +0200

    Optimized admin layout and extracted the different sections to partials

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,64 +1,17 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
   <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="icon" type="image/svg+xml" href="/img/scribble-bold.svg" />
-@if (file_exists(public_path('hot')))
-  @vite(['resources/css/app.module.styl', 'resources/js/app.js'])
-@else
-@php
-  $manifest = json_decode(file_get_contents(public_path('build/manifest.json')), true);
-
-  //echo("var_dump \$manifest:<br>\n");
-  //var_dump($manifest);
-  //echo("<br>\n");
-  //echo("print_r \$manifest:<br>\n");
-  //print_r($manifest);
-  //echo("<br>\n");
-
-  $cssPath = $manifest['resources/css/app.module.styl']['css'][0] ?? null;
-  $cssAssetPath = asset("build/$cssPath");
-  //$jsPath = $manifest['resources/js/app.js']['file'] ?? null;
-  $jsAssetPath = Vite::asset('resources/js/app.js');
-@endphp
-@if ($cssAssetPath)
-    <link rel="stylesheet" href="{{ $cssAssetPath }}">
-@endif
-@if ($jsAssetPath)
-    <script type="module" src="{{ $jsAssetPath }}"></script>
-@endif
-@endif
-    <title>Admin - {{ config('app.name', 'Scribble! (from Blade)') }}</title>
+    @include('partials._head', ['pageTitle' => 'Admin'])
   </head>
   <body>
     <header class="header">
-      <nav class="main-nav">
-        <div style="display: flex; justify-content: space-between; align-items: center">
-          <div style="font-size: 2rem; font-weight: 600">
-            <a href="{{ route('admin.index') }}" title="To Admin Start Page">Admin</a>
-          </div>
-          <div>Right part of header</div>
-        </div>
-      </nav>
-    @auth
-      <a href="{{ url('/dashboard') }}" class="dashboard">Dashboard </a>
-    @if (Route::has('login'))
-      <a href="{{ route('login') }}" class="login">Log in</a>
-    @endif
-    @if (Route::has('register'))
-      <a href="{{ route('register') }}" class="register">Register</a>
-    @endif
-    @endauth
-</header>
-    <main class="container">
+      @include('partials._header')
+    </header>
+    <main class="main">
       @yield('content')
     </main>
-    <footer>
-      <div style="display: flex; justify-content: space-between; align-items: center; margin: 1rem 0;">
-        <div>&copy; {{ now()->year }} <a href="mailto: wallindev@gmail.com">wallindev</a></div>
-        <div>Right part of footer</div>
-      </div>
+    <footer class="footer">
+      @include('partials._footer')
     </footer>
   </body>
 </html>
