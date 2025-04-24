a04529dff7674972d63b482a200afe2e72d087d5
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-04-24 21:57:11 +0200

    Optimized IsAdmin middleware

--- app/Http/Middleware/IsAdmin.php
+++ app/Http/Middleware/IsAdmin.php
@@ -2,23 +2,10 @@
 
 namespace App\Http\Middleware;
 
-use Closure;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-
 class IsAdmin {
-  /**
-   * Handle an incoming request.
-   *
-   * @param  \Illuminate\Http\Request  $request
-   * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
-   * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
-   */
-  public function handle(Request $request, Closure $next) {
-    if (Auth::guard('admin')->check() && Auth::guard('admin')->user()->is_admin) {
-      return $next($request);
+  public function handle() {
+    if (!(auth('admin')->check() && auth('admin')->user()->is_admin)) {
+      return redirect()->route('login.show')->with('error', 'Administrator privileges required.');
     }
-
-    return redirect('/admin/login')->with('error', 'Administrator privileges required.');
   }
 }
