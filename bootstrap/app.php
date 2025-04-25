4d7e5c81dc65fedb3b20d0e912e7bcdba9e6a1c4
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-04-25 22:02:28 +0200

    Added middleware alias "admin" (IsAdmin class)

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\IsAdmin;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -12,7 +13,13 @@ return Application::configure(basePath: dirname(__DIR__))
     health: '/up',
   )
   ->withMiddleware(function (Middleware $middleware) {
-    //
+    $middleware->alias([
+      'admin' => IsAdmin::class,
+      // ... other middleware aliases
+    ]);
+
+    // You might also append it to specific middleware groups if needed
+    // $middleware->get('web')->push(IsAdmin::class);
   })
   ->withExceptions(function (Exceptions $exceptions) {
     //
