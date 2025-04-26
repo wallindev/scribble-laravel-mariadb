bd4f273c5aa44eaaff0c0f954026880738cccc09
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-04-26 22:09:36 +0200

    Moved auth partial to right part of main navigation in header

--- resources/views/partials/_header.blade.php
+++ resources/views/partials/_header.blade.php
@@ -2,6 +2,5 @@
         <div style="font-size: 1rem; background-color: #1a1a1a; padding: .25rem .5rem; border-radius: .25rem;">
           @include('partials._breadcrumbs')
         </div>
-        <div>Right part of header</div>
+        <div>@include('partials._auth')</div>
       </nav>
-      @include('partials._auth')
