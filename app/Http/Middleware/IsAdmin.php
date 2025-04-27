604b9525cb367ca93b4b4762e23895c34cbbe3d4
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-04-27 22:20:52 +0200

    Put back Closure to resume the request if passing the check

--- app/Http/Middleware/IsAdmin.php
+++ app/Http/Middleware/IsAdmin.php
@@ -2,10 +2,14 @@
 
 namespace App\Http\Middleware;
 
+use Closure;
+use Illuminate\Http\Request;
+
 class IsAdmin {
-  public function handle() {
+  public function handle(Request $request, Closure $next) {
     if (!(auth('admin')->check() && auth('admin')->user()->is_admin)) {
       return redirect()->route('login.show')->with('error', 'Administrator privileges required.');
     }
+    return $next($request);
   }
 }
