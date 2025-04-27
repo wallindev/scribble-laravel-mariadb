25ec991a5a26f673be42513c4242e70bf89fa226
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-04-27 23:27:14 +0200

    Beautified, optimized, and added function isAuthenticated

--- app/Http/Controllers/Admin/AdminAuthController.php
+++ app/Http/Controllers/Admin/AdminAuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class AdminAuthController extends AdminController {
   public function show() {
@@ -15,26 +16,30 @@ class AdminAuthController extends AdminController {
     return view('admin.login.show', compact('title', 'breadcrumbs'));
   }
 
-  public function login(Request $request) {
+  public function login(Request $request): Response {
     $credentials = $request->only('email', 'password');
 
-    if (auth('admin')->attempt($credentials, $request->filled('remember'))) {
-      $user = auth('admin')->user();
+    if (!auth('admin')->attempt($credentials, $request->filled('remember'))) {
+      return back()->withErrors(['email' => 'Wrong email or password.'])->onlyInput('email');
+    }
 
-      if ($user && $user->is_admin) {
-        $request->session()->regenerate();
-        return redirect()->intended(route('admin.index'));
-      } else {
-        auth('admin')->logout();
-        return back()->withErrors(['email' => 'You do not have administrator privileges.'])->onlyInput('email');
-      }
+    if (auth('admin')->user()?->is_admin) {
+      $request->session()->regenerate();
+      return redirect()->intended(route('admin.index'));
+    } else {
+      auth('admin')->logout();
+      return back()->withErrors(['email' => 'You do not have administrator privileges.'])->onlyInput('email');
     }
   }
 
-  public function logout(Request $request) {
+  public function logout(Request $request): Response {
     auth('admin')->logout();
     $request->session()->invalidate();
     $request->session()->regenerateToken();
     return redirect()->route('login.show');
+  }
+
+  public function isAuthenticated(): bool {
+    return auth('admin')->check();
   }
 }
