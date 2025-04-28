18bdfb14b42a560c166bcf518e611371cdc80ffd
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-04-28 16:33:44 +0200

    Added full name and logout form

--- resources/views/partials/_auth.blade.php
+++ resources/views/partials/_auth.blade.php
@@ -1,9 +1,17 @@
-    @auth
-      <a href="{{ url('/dashboard') }}" class="dashboard">Dashboard </a>
-    @if (Route::has('login'))
-      <a href="{{ route('login') }}" class="login">Log in</a>
-    @endif
-    @if (Route::has('register'))
-      <a href="{{ route('register') }}" class="register">Register</a>
-    @endif
+    @auth('admin')
+      @if (auth('admin')->check())
+        <!--Name: -->{{ auth('admin')->user()->fullName }}<br>
+        @if (auth('admin')->user()->is_admin)
+          Administrator<br>
+        @endif
+        <form action="{{ route('admin.logout') }}" method="POST">
+          @csrf
+          @method('POST')
+          <button type="submit">Log Out</button>
+        </form>
+      @else
+        <a class="link-button" href="{{ route('login.show') }}">Log In</a><br>
+      @endif
+    @else
+      Name: guest
     @endauth
