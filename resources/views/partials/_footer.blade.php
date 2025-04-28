0eaa62619f1381d48accf373ad36eddeb3113207
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-04-28 16:34:49 +0200

    Changed inline styling to class "wrapper"

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,4 +1,4 @@
-      <div style="display: flex; justify-content: space-between; align-items: center; margin: 1rem 0;">
+      <div class="wrapper">
         <div>&copy; {{ now()->year }} <a href="mailto: wallindev@gmail.com">wallindev</a></div>
-        <div>Right part of footer</div>
+        <div><!--Right part of footer--></div>
       </div>
