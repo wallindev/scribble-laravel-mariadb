dc1d4a1b7b399058bb05bfefa0b7e9b8b75928ff
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-04-28 16:35:31 +0200

    Changed inline styling to classes "breadcrumbs" and "auth"

--- resources/views/partials/_header.blade.php
+++ resources/views/partials/_header.blade.php
@@ -1,6 +1,6 @@
       <nav class="main-nav">
-        <div style="font-size: 1rem; background-color: #1a1a1a; padding: .25rem .5rem; border-radius: .25rem;">
+        <div class="breadcrumbs">
           @include('partials._breadcrumbs')
         </div>
-        <div>@include('partials._auth')</div>
+        <div class="auth">@include('partials._auth')</div>
       </nav>
