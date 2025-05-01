f68e8d3a7d1d65b41774cf641641141d3c46de2f
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-05-01 12:11:47 +0200

    Instead of checking authentication (which is done in the login functionality) and if user is an admin, this middleware now only checks if user is an admin

--- app/Http/Middleware/IsAdmin.php
+++ app/Http/Middleware/IsAdmin.php
@@ -8,9 +8,17 @@ use Symfony\Component\HttpFoundation\Response;
 
 class IsAdmin {
   public function handle(Request $request, Closure $next): Response {
-    if (!(auth('admin')->check() && auth('admin')->user()?->is_admin)) {
-      return redirect()->route('login.show')->with('error', 'Administrator privileges required.');
+    // Exclude the login POST request (that has its own auth logic)
+    // if ($request->is(route('auth.login')) && $request->isMethod('post')) {
+    //   return $next($request);
+    // }
+
+    // If user is authenticated but not admin, redirect to home
+    if (!auth('admin')->user()?->is_admin) {
+      return redirect()->route('home.index')->withErrors(['error' => 'Administrator privileges required.']);
     }
+
+    // Otherwise return the request unaffected
     return $next($request);
   }
 }
