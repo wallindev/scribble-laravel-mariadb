db75e035b40ec1c52a9bb850d5f54754b2f90cc4
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-05-03 13:03:04 +0200

    Removed Bootstrap CDN

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -1,11 +1,10 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="icon" type="image/svg+xml" href="/img/scribble-bold.svg" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
-@if (file_exists(public_path('hot')))
+  @if (file_exists(public_path('hot')))
     @vite(['resources/css/app.styl', 'resources/js/app.js'])
-@else
+  @else
     <link rel="stylesheet" href="{{ Vite::asset('resources/css/app.styl') }}">
     <script type="module" src="{{ Vite::asset('resources/js/app.js') }}"></script>
-@endif
+  @endif
     <title>{{ $pageTitle }} - {{ config('app.name', 'Scribble! (from Blade)') }}</title>
