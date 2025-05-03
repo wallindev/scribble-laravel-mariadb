88b29dbcd7dd06aa12b1da332a73d3c282682756
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-05-03 13:16:00 +0200

    Placed the info and logout button on the same row to better utilize the space

--- resources/views/partials/_auth.blade.php
+++ resources/views/partials/_auth.blade.php
@@ -1,17 +1,17 @@
     @auth('admin')
       @if (auth('admin')->check())
-        <!--Name: -->{{ auth('admin')->user()->fullName }}<br>
+        User: {{ auth('admin')->user()->fullName }} &lt;{{ auth('admin')->user()->email }}&gt;
         @if (auth('admin')->user()->is_admin)
-          Administrator<br>
+          [Administrator]
         @endif
-        <form action="{{ route('admin.logout') }}" method="POST">
+        | <form action="{{ route('auth.logout') }}" method="POST" style="display: inline">
           @csrf
           @method('POST')
           <button type="submit">Log Out</button>
         </form>
       @else
-        <a class="link-button" href="{{ route('login.show') }}">Log In</a><br>
+        <a class="link-button" href="{{ route('auth.index') }}">Log In</a><br>
       @endif
     @else
-      Name: guest
+      User: guest
     @endauth
