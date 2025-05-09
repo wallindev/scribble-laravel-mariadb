21054c69a808cdf086058c1f06d0eb390975d0d0
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-05-09 18:53:13 +0200

    Removed sessions table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -20,15 +20,6 @@ return new class extends Migration {
       $table->rememberToken();
       $table->timestamps();
     });
-
-    Schema::create('sessions', function (Blueprint $table) {
-      $table->string('id')->primary();
-      $table->foreignId('user_id')->nullable()->index();
-      $table->string('ip_address', 45)->nullable();
-      $table->text('user_agent')->nullable();
-      $table->longText('payload');
-      $table->integer('last_activity')->index();
-    });
   }
 
   /**
@@ -36,7 +27,5 @@ return new class extends Migration {
    */
   public function down(): void {
     Schema::dropIfExists('users');
-    Schema::dropIfExists('password_reset_tokens');
-    Schema::dropIfExists('sessions');
   }
 };
