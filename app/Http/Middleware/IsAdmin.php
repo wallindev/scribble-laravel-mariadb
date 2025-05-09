c592dc441def32e871e11f768eabd936df701be3
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-05-09 18:54:07 +0200

    Removed old unused code

--- app/Http/Middleware/IsAdmin.php
+++ app/Http/Middleware/IsAdmin.php
@@ -8,11 +8,6 @@ use Symfony\Component\HttpFoundation\Response;
 
 class IsAdmin {
   public function handle(Request $request, Closure $next): Response {
-    // Exclude the login POST request (that has its own auth logic)
-    // if ($request->is(route('auth.login')) && $request->isMethod('post')) {
-    //   return $next($request);
-    // }
-
     // If user is authenticated but not admin, redirect to home
     if (!auth('admin')->user()?->is_admin) {
       return redirect()->route('home.index')->withErrors(['error' => 'Administrator privileges required.']);
