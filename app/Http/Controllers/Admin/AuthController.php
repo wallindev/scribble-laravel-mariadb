1418ca0a5b7e77a4674a440aaa231c3844825fc8
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-05-09 18:56:18 +0200

    Removed all dump():s and commented code

--- app/Http/Controllers/Admin/AuthController.php
+++ app/Http/Controllers/Admin/AuthController.php
@@ -13,15 +13,12 @@ class AuthController extends AdminController {
   }
 
   private function logoutAdmin(): void {
-    // dump('before auth(\'admin\')->logout()');
     auth('admin')->logout();
     $this->request->session()->invalidate();
     $this->request->session()->regenerateToken();
-    // dump('after auth(\'admin\')->logout()');
   }
 
   public function index() {
-    // dump(session('url.intended'));
     $title = 'Log In';
     $breadcrumbs = [
       ...$this->breadcrumbs,
@@ -38,11 +35,6 @@ class AuthController extends AdminController {
       return back()->withErrors(['email' => 'Wrong email or password.'])->onlyInput('email');
     }
 
-    // if (!auth('admin')->user()?->is_admin) {
-    //   $this->logoutAdmin();
-    //   return back()->withErrors(['email' => 'You do not have administrator privileges.'])->onlyInput('email');
-    // }
-
     // Use regenerate() or regenerateToken()?
     // $this->request->session()->regenerateToken();
     $this->request->session()->regenerate();
@@ -56,9 +48,7 @@ class AuthController extends AdminController {
   }
 
   public function logout(): RedirectResponse {
-    // dump('function logout()');
     $this->logoutAdmin();
-    // dd('after $this->logoutAdmin() in function logout()');
     return redirect()->route('auth.index');
   }
 }
