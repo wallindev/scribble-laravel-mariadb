55d24be577fd2e9618f2f01b82d8763365c8d3be
Mikael Wallin <9991589+wallindev@users.noreply.github.com>
2025-05-19 22:00:01 +0200

    Updated and optimized routes

--- routes/web.php
+++ routes/web.php
@@ -7,73 +7,39 @@ use App\Http\Controllers\Admin\HomeController as AdminHomeController;
 use Illuminate\Support\Facades\Route;
 
 // Start/Root Page, this is going to serve the React SPA (this is public, no restrictions)
-Route::get('/', [HomeController::class, 'index'])->name('home.index');
+Route::resource('/', HomeController::class)->only('index')->name('index', 'home.index');
+// Route::get('/', [HomeController::class, 'index'])->name('home.index');
 
 // Everything from "/admin" path and beyond
 Route::prefix('admin')->group(function () {
   // Unauthenticated users ("guests" in the "admin" guard) have access
   Route::middleware('guest:admin')->group(function () {
-    Route::get('/login', [AuthController::class, 'index'])->name('auth.index');
-    Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
+    Route::get('login', [AuthController::class, 'index'])->name('auth.index');
+    Route::post('login', [AuthController::class, 'login'])->name('auth.login');
   });
 
   // Authenticated users that are NOT admin have access ("users" in the "admin" guard with is_admin=false)
   Route::middleware('auth:admin')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout'])->name('auth.logout');
+    Route::post('logout', [AuthController::class, 'logout'])->name('auth.logout');
   });
 
   // Authenticated users that ARE admin have access ("users" in the "admin" guard with is_admin=true)
   Route::middleware(['auth:admin', 'admin'])->group(function () {
     // Admin Start Page
-    Route::get('/', [AdminHomeController::class, 'index'])->name('admin.index');
+    Route::resource('/', AdminHomeController::class)->only('index')->name('index', 'admin.index');
+    // Route::get('/', [AdminHomeController::class, 'index'])->name('admin.index');
 
     /*
     * Users
     */
-    // List users
-    Route::get('users', [UserController::class, 'index'])->name('users.index');
-
-    // Show user
-    Route::get('users/{id}', [UserController::class, 'show'])->name('users.show');
-
-    // Edit user
-    Route::get('users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
-
-    // Update user
-    Route::patch('users/{id}', [UserController::class, 'update'])->name('users.update');
-
-    // Create user
     Route::get('users/new', [UserController::class, 'create'])->name('users.create');
-
-    // Store user
-    Route::post('users', [UserController::class, 'store'])->name('users.store');
-
-    // Remove/delete user
-    Route::delete('users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
+    Route::resource('users', UserController::class);
 
     /*
     * Articles
     */
-    // List articles
-    Route::get('articles', [ArticleController::class, 'index'])->name('articles.index');
-
-    // Show article
-    Route::get('articles/{id}', [ArticleController::class, 'show'])->name('articles.show');
-
-    // Edit article
-    Route::get('articles/{id}/edit', [ArticleController::class, 'edit'])->name('articles.edit');
-
-    // Update article
-    Route::put('articles/{id}', [ArticleController::class, 'update'])->name('articles.update');
-
-    // Create article
     Route::get('articles/new', [ArticleController::class, 'create'])->name('articles.create');
-
-    // Store article
-    Route::post('articles', [ArticleController::class, 'store'])->name('articles.store');
-
-    // Remove/delete article
-    Route::delete('articles/{id}', [ArticleController::class, 'destroy'])->name('articles.destroy');
+    Route::resource('articles', ArticleController::class)->except('create');
   });
 
   // Catch-all for admin, to avoid "HTTP 404 Not Found" generic error page
@@ -83,6 +49,6 @@ Route::prefix('admin')->group(function () {
 });
 
 // Catch-all for root, to avoid "HTTP 404 Not Found" generic error page
-Route::any('{any}', function () {
-  return redirect()->route('home.index')->withErrors(['error' => 'Page does not exist.']);
-})->where('any', '.*');
+// Route::any('{any}', function () {
+//   return redirect()->route('home.index')->withErrors(['error' => 'Page does not exist.']);
+// })->where('any', '.*');
